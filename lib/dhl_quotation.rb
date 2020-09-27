bda3c23c2e351badc412829a074a981049b030c3
Yuri <yuri.dias@gocase.com.br>
2020-09-27 19:28:17 -0300

    better configuration class

--- lib/dhl_quotation.rb
+++ lib/dhl_quotation.rb
@@ -6,3 +6,22 @@ require 'dhl_quotation/express/payload'
 require 'dhl_quotation/express/decoder'
 require 'dhl_quotation/express/importer'
 require 'dhl_quotation/express/world_importer'
+
+module DHLQuotation
+  class << self
+    def configure
+      yield configuration
+    end
+
+    def configuration
+      @configuration ||= Configuration.new
+    end
+  end
+end
+
+require 'singleton'
+class Configuration
+  attr_accessor :user, :password
+  attr_accessor :account
+  attr_accessor :taxable_countries
+end
